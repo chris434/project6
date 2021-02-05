8cc17aad32758d07e47d8c854ed233b4774bc1b9
chris434 <christopherpearce589@gmail.com>
2021-02-05 13:07:40 +0000

    server set up

--- back-end/server.js
+++ back-end/server.js
@@ -1,1 +1,7 @@
+const express = require('express')
 require('./db/db_config')
+const server = express()
+const port = process.env.PORT || 3000
+server.listen(port, () => {
+    console.log('listening on port' + port)
+})
